2209fd4c49e55f262087c62a7ad293beefab6cd2
emin-aze-mm <emin.mammadzada.2021@gmail.com>
2023-07-29 22:47:10 +0400

    added

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { User } from '@prisma/client';
+import Container from '../Container'
+import Logo from './Logo'
+import Search from './Search'
+import UserMenu from './UserMenu'
+import { SafeUser } from '@/app/types';
+
+interface NavbarProps{
+ currentUser?: SafeUser | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser
+}) => {
+  
+  console.log({currentUser})
+  return (
+    <div className='fixed w-full bg-white z-10 shadow-sm'>
+        <div className='py-4 border-b-[1px]'>
+            <Container>
+                <div className='flex flex-row items-center justify-between gap-3 md:gap-0'>
+                    <Logo />
+                    <Search/>
+                    <UserMenu currentUser = {currentUser}/>
+                </div>
+            </Container>
+        </div>
+    </div>
+  )
+}
+
+console.log(process.env.GITHUB_ID);
+console.log(process.env.NEXT_GITHUB_ID)
+console.log(process.env.NEXT_PUBLIC_NEXTAUTH_SECRET)
+
+console.log(process.env.NEXT_PUBLIC_GITHUB_ID);
+console.log(process.env.NEXT_PUBLIC_GITHUB_SECRET);
+
+export default Navbar
